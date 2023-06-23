35d394b2120fbc849912fb2cbdebc1592ede2b67
MaheshwariKunj <137528002+MaheshwariKunj@users.noreply.github.com>
2023-06-23 21:36:03 +0530

    1

---
+++ Loops/Printingnumbers.java
@@ -0,0 +1,9 @@
+class Printingnumbers{
+    public static void main(String args[]){
+        int counter = 1;
+        while(counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
